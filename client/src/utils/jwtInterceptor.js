c09bba88e68ce6a353b294b9420b5d4d130230c9
datfoi <jengjiening@gmail.com>
2024-01-18 14:57:57 +0700

    feat: add hh2 complete assignment

--- client/src/utils/jwtInterceptor.js
+++ client/src/utils/jwtInterceptor.js
@@ -2,13 +2,34 @@ import axios from "axios";
 
 function jwtInterceptor() {
   axios.interceptors.request.use((req) => {
-    // 🐨 Todo: Exercise #6
-    //  ให้เขียน Logic ในการแนบ Token เข้าไปใน Header ของ Request
-    // เมื่อมีการส่ง Request จาก Client ไปหา Server
-    // ภายใน Callback Function axios.interceptors.request.use
+    axios.interceptors.request.use((req) => {
+      const hasToken = Boolean(window.localStorage.getItem("token"));
+
+      if (hasToken) {
+        req.headers = {
+          ...req.headers,
+          Authorization: `Bearer ${window.localStorage.getItem("token")}`,
+        };
+      }
+    });
 
     return req;
   });
+
+  axios.interceptors.response.use(
+    (req) => {
+      return req;
+    },
+    (error) => {
+      if (
+        error.response.status === 401 &&
+        error.response.statusText === "Unauthorized"
+      ) {
+        window.localStorage.removeItem("token");
+        window.location.replace("/");
+      }
+    }
+  );
 
   axios.interceptors.response.use(
     (req) => {
